14ab0ca22f515eb9e5cde16e0c814ddf0164a7e2
mokhs00 <mokhs00@naver.com>
2021-07-18 22:25:35 +0900

    add : spring-exception best practice

--- src/main/java/com/mokhs/springplayground/springexception/advice/GlobalControllerAdvice.java
+++ src/main/java/com/mokhs/springplayground/springexception/advice/GlobalControllerAdvice.java
@@ -1,9 +1,24 @@
 package com.mokhs.springplayground.springexception.advice;
 
+import com.mokhs.springplayground.springexception.dto.Error;
+import com.mokhs.springplayground.springexception.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
@@ -18,8 +33,92 @@ public class GlobalControllerAdvice {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
         System.out.println(e.getLocalizedMessage());
-        return ResponseEntity.badRequest().body(e.getMessage());
+
+        List<Error> errorList = new ArrayList<>();
+
+        BindingResult bindingResult = e.getBindingResult();
+        bindingResult.getAllErrors().forEach(error -> {
+            FieldError fieldError = (FieldError) error;
+
+            String fieldName = fieldError.getField();
+            String message = fieldError.getDefaultMessage();
+            String value = fieldError.getRejectedValue().toString();
+
+            Error errorMessage = new Error();
+            errorMessage.setMessage(message);
+            errorMessage.setField(fieldName);
+            errorMessage.setInvalidValue(value);
+
+            errorList.add(errorMessage);
+        });
+
+        ErrorResponse response = new ErrorResponse();
+        response.setErrorList(errorList);
+        response.setMessage("");
+        response.setRequestUrl(request.getRequestURI());
+        response.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        response.setResultCode("FAIL");
+        return ResponseEntity.badRequest()
+                .body(response);
+    }
+
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity handleConstraintViolationException(ConstraintViolationException e, HttpServletRequest request) {
+        System.out.println(e.getLocalizedMessage());
+
+        List<Error> errorList = new ArrayList<>();
+
+        e.getConstraintViolations().forEach(error -> {
+            Stream<Path.Node> stream = StreamSupport.stream(error.getPropertyPath().spliterator(), false);
+            List<Path.Node> list = stream.collect(Collectors.toList());
+
+            String field = list.get(list.size() - 1).getName();
+            String message = error.getMessage();
+            String invalidValue = error.getInvalidValue().toString();
+
+            Error errorMessage = new Error();
+            errorMessage.setMessage(message);
+            errorMessage.setField(field);
+            errorMessage.setInvalidValue(invalidValue);
+
+            errorList.add(errorMessage);
+        });
+
+        ErrorResponse response = new ErrorResponse();
+        response.setErrorList(errorList);
+        response.setMessage("");
+        response.setRequestUrl(request.getRequestURI());
+        response.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        response.setResultCode("FAIL");
+
+        return ResponseEntity.badRequest()
+                .body(response);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity handleMissingServletRequestParameterException(MissingServletRequestParameterException e, HttpServletRequest request) {
+        System.out.println(e.getLocalizedMessage());
+
+        List<Error> errorList = new ArrayList<>();
+
+        String fieldName = e.getParameterName();
+        String message = e.getMessage();
+
+        Error errorMessage = new Error();
+        errorMessage.setField(fieldName);
+        errorMessage.setMessage(message);
+
+        ErrorResponse response = new ErrorResponse();
+        response.setErrorList(errorList);
+        response.setMessage("");
+        response.setRequestUrl(request.getRequestURI());
+        response.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        response.setResultCode("FAIL");
+
+        return ResponseEntity.badRequest()
+                .body(response);
     }
 }
--- src/main/java/com/mokhs/springplayground/springexception/controller/Api.java
+++ src/main/java/com/mokhs/springplayground/springexception/controller/Api.java
@@ -1,16 +1,21 @@
 package com.mokhs.springplayground.springexception.controller;
 
 import com.mokhs.springplayground.springexception.dto.User;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/v1/user")
+@Validated
 public class Api {
 
     @GetMapping
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    public User get(@RequestParam @NotBlank String name,
+                    @RequestParam @Min(1) Integer age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
